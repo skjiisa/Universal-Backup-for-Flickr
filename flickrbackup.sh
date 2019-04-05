d02d328b13a630ccb16c36d6dafc1968fe131243
Isvvc <lyons@tuta.io>
2019-04-04 18:35:39 -0600

    Make default mp4 a whole number of blocks
    Make the output.mp4 exactly 1536 bytes (up from 1506) to make it a multiple of the standard block size of 512. Remove `oflag=seek_bytes` to improve compatibility

--- flickrbackup.sh
+++ flickrbackup.sh
@@ -8,11 +8,15 @@ if [ ! -d "$1" ]; then
 	exit 1
 fi
 
-7za a "$dirname$datetime.7z" "$1" -p -mhe -mx=0 -v1000m
+eval 7za a "$dirname$datetime.7z" "$1" -p -mhe -mx=0 -v1000m
+
+if [ $? -ne 0 ]; then
+	exit 1
+fi
 
 for i in $dirname$datetime.7z.00*; do
 	echo "Uploading $i"
-	cp ~/output.mp4 $i.mp4
-	dd if=$i of=$i.mp4 seek=1506 oflag=seek_bytes
-	flickcurl upload $i.mp4
+	cp output.mp4 $i.mp4
+	dd if=$i of=$i.mp4 bs=512 seek=3
+	flickcurl -V upload $i.mp4
 done
